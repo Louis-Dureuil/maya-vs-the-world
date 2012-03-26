8e876d7954c4b1577e997751da0b306854f6d226
dureuill <louis.dureuil@ensimag.imag.fr>
2012-03-26 15:59:36 +0200

    La hiérarchie de projet est maintenant correcte.

---
+++ maya-vs-the-world/Rpg/Rpg/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using LuxEngine;
+
+namespace Schmup
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (LuxGame game = new LuxGame())
+            {
+                //game.Components.Add(new Scenes.DemoTitleScene(game));
+                game.Run();
+            }
+        }
+    }
+#endif
+}
