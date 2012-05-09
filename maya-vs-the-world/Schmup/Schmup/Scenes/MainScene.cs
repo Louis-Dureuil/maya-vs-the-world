ef4fd0021149e266ce39811da14a2adf903fcc76
dureuill <louis.dureuil@ensimag.imag.fr>
2012-05-09 20:00:44 +0200

    Correction du bug signalé

--- maya-vs-the-world/Schmup/Schmup/Scenes/MainScene.cs
+++ maya-vs-the-world/Schmup/Schmup/Scenes/MainScene.cs
@@ -11,9 +11,21 @@ namespace Schmup
         public MainScene(LuxGame game)
             : base(game)
         {
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
             List<string> skinName = new List<string>(1);
             skinName.Add("carre");
-            Hero hero = new Hero(game, 1, 0, 0, new Sprite(this, skinName));
+            Sprite heroSprite = new Sprite(this, skinName);
+            // Les creations de sprite doivent être dans Initialize.
+            Hero hero = new Hero(this.LuxGame, 1, 0, 0, heroSprite);
+            // Il faut ajouter au jeu les élements que vous créez.
+            Game.Components.Add(hero);
+            Game.Components.Add(heroSprite);
+            // Il faut appliquer "SetAnimation" au sprite pour qu'il affiche quelque chose.
+            heroSprite.SetAnimation("carre");
         }
     }
 }
