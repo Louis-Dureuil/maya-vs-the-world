d2eb1807afc073fce13520215b59a5af4144e65c
unknown <Alexis@PC-de-Qlex.(none)>
2012-05-24 14:28:10 +0200

    shotPattern marche en accord avec le keikaku (AAAWWW YEAAAA). + TestEnemy.cs cree

---
+++ maya-vs-the-world/Schmup/Schmup/Scenes/TestEnemy.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using LuxEngine;
+
+namespace Schmup
+{
+    class TestEnemy : Enemy
+    {
+        private bool shootsHero;
+        private uint waitTimeFrames;
+        private uint shootNb;
+
+        public TestEnemy (LuxGame game, uint life, uint takenDamageCollision, uint givenDamageCollision, bool shootsHero, uint waitTimeFrames, Sprite skin)
+            : base(game, life, takenDamageCollision, givenDamageCollision, skin)
+        {
+            this.shootsHero = shootsHero;
+            this.waitTimeFrames = waitTimeFrames;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            List<string> skinName = new List<string>(1);
+            skinName.Add("bullet001-1");
+            Vector2 vector = new Vector2(0,1);
+            if (shootsHero)
+            {
+                Vector2 vect = new Vector2(1, 1);
+            }
+            Shot shot = new Shot(this.LuxGame, 1, null);
+            Sprite shotSprite = new Sprite(shot, skinName);
+            shot.Speed = vector;
+            shot.Skin = shotSprite;
+            shot.Position = this.Position;
+
+            Game.Components.Add(shot);
+            Game.Components.Add(shotSprite);
+            shotSprite.SetAnimation("bullet001-1");
+            shootNb=1;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (waitTimeFrames == 0)
+            {
+                shootNb++;
+                Vector2 vector = new Vector2(0,1);
+                if (shootsHero)
+                {
+                    Vector2 vect = new Vector2(1, 1);
+                }
+                ShotPattern bPatternTest = new ShotPattern(this.LuxGame, shootNb, vector, 12);
+                bPatternTest.Position = this.Position;
+                Game.Components.Add(bPatternTest);
+                waitTimeFrames += 60;
+            }
+            waitTimeFrames--;
+        }
+    }
+}
