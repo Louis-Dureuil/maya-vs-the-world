deecbbe3527a699b0ed1c8c530ed76957ed0e13d
unknown <Alexis@PC-de-Qlex.(none)>
2012-06-25 20:11:59 +0200

    projet test : le heros se deplace et un ennemi lui tire dessus

--- maya-vs-the-world/Schmup/Schmup/Scenes/MainScene.cs
+++ maya-vs-the-world/Schmup/Schmup/Scenes/MainScene.cs
@@ -21,36 +21,27 @@ namespace Schmup
             skinName.Add("carre");
             skinName.Add("bullet001-1");
             skinName.Add("bullet001-2");
+            skinName.Add("hero");
             Vector2 vect = new Vector2(0,1);
             Vector2 rien = new Vector2(0,0);
             // Les creations de sprite doivent être dans Initialize.
             
-            ShotPattern bPatternTest = new ShotPattern(this.LuxGame, 7, vect, 10);
-            bPatternTest.Position = new Vector2(300, 300);
             Hero hero = new Hero(this.LuxGame, 1, 0, 0, null, 5);
             Sprite heroSprite = new Sprite(hero, skinName);
-            Shot shot = new Shot(this.LuxGame, 1, null);
-            Sprite shotSprite = new Sprite(shot, skinName);
             TestEnemy testEnemy = new TestEnemy(this.LuxGame, 10, 2, 3, false, 100, null);
             Sprite enemySprite = new Sprite(testEnemy, skinName);
-
-            shot.Position = bPatternTest.Position;
-            shot.Speed = rien;
-            shot.Skin = shotSprite;
-            testEnemy.Position = new Vector2(400, 100);
+            
+            testEnemy.Position = new Vector2(0,0);
             testEnemy.Skin = enemySprite;
+            hero.Position = new Vector2(400, 200);
             hero.Skin = heroSprite;
             // Il faut ajouter au jeu les élements que vous créez.
             Game.Components.Add(hero);
             Game.Components.Add(heroSprite);
-            Game.Components.Add(shot);
-            Game.Components.Add(shotSprite);
-            Game.Components.Add(bPatternTest);
             Game.Components.Add(testEnemy);
             // Il faut appliquer "SetAnimation" au sprite pour qu'il affiche quelque chose.
 
-            heroSprite.SetAnimation("bullet001-2");
-            shotSprite.SetAnimation("bullet001-1");
+            heroSprite.SetAnimation("hero");
             enemySprite.SetAnimation("carre");
         }
 
--- maya-vs-the-world/Schmup/Schmup/Scenes/TestEnemy.cs
+++ maya-vs-the-world/Schmup/Schmup/Scenes/TestEnemy.cs
@@ -13,6 +13,8 @@ namespace Schmup
         private uint waitTimeFrames;
         private uint shootNb;
         private float shootSpeed;
+        private Vector2 speed = new Vector2((float) 2,(float) 1);
+        private Vector2 accel = new Vector2(0,(float) -0.01);
 
         public TestEnemy (LuxGame game, uint life, uint takenDamageCollision, uint givenDamageCollision, bool shootsHero, uint waitTimeFrames, Sprite skin)
             : base(game, life, takenDamageCollision, givenDamageCollision, skin)
@@ -26,42 +28,35 @@ namespace Schmup
             base.Initialize();
             List<string> skinName = new List<string>(1);
             skinName.Add("bullet001-1");
-            Vector2 vector = new Vector2(0,1);
+            Vector2 vector = new Vector2(0,5);
             if (shootsHero)
             {
                 Vector2 vect = new Vector2(1, 1);
             }
-            Shot shot = new Shot(this.LuxGame, 1, null);
-            Sprite shotSprite = new Sprite(shot, skinName);
-            shot.Speed = vector;
-            shot.Skin = shotSprite;
-            shot.Position = this.Position;
-
-            Game.Components.Add(shot);
-            Game.Components.Add(shotSprite);
-            shotSprite.SetAnimation("bullet001-1");
             shootNb=1;
-            shootSpeed = (float) 0.05;
+            shootSpeed = (float) 0.5;
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-            if (shootNb < 30)
+            this.Position += speed;
+            this.speed += accel;
+            if (shootNb < 20)
             {
                 if (waitTimeFrames == 0)
                 {
                     shootNb++;
-                    shootSpeed = (float) 1.1 * shootSpeed + (float) 0.1;
+                    shootSpeed = (float) 1.1 * shootSpeed + (float) 0.15;
                     Vector2 vector = new Vector2(0,(float) shootSpeed);
                     if (shootsHero)
                     {
                         Vector2 vect = new Vector2(1, 1);
                     }
-                    ShotPattern bPatternTest = new ShotPattern(this.LuxGame, 7, vector, 12);
+                    ShotPattern bPatternTest = new ShotPattern(this.LuxGame, 4, vector, 40);
                     bPatternTest.Position = this.Position;
                     Game.Components.Add(bPatternTest);
-                    waitTimeFrames += 3;
+                    waitTimeFrames += 4;
                 }
                 waitTimeFrames--;
             }
