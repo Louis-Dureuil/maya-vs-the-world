677ca1abfec91a4f4c183fbf73d1fb43459f0834
unknown <jacob_alexis@hotmail.com>
2013-03-30 17:11:04 +0100

    legere modification

--- maya-vs-the-world/Schmup/Schmup/Scenes/ShootsHero.cs
+++ maya-vs-the-world/Schmup/Schmup/Scenes/ShootsHero.cs
@@ -24,11 +24,21 @@ namespace Schmup
         public override void Update(GameTime gameTime)
         {
             elapsedSec += gameTime.ElapsedGameTime.TotalSeconds;
-            if (elapsedSec > 0.3)
+            if (elapsedSec > 0.3 && enemy.Life > 0 && Vector2.Distance(enemy.Position,world.Hero.Position) > 50)
             {
                 elapsedSec = 0;
                 direction = 4 * Vector2.Normalize(world.Hero.Position - enemy.Position);
                 shots.Shoot(enemy.Position.X, enemy.Position.Y, direction.X, direction.Y, 0, 0, false, false, false);
+            }
+            if (enemy.Life <= 0 && elapsedSec > 0)
+            {
+                elapsedSec = -100;
+                direction = 4 * Vector2.Normalize(world.Hero.Position - enemy.Position);
+                shots.Shoot(enemy.Position.X, enemy.Position.Y, 3*direction.X, 3*direction.Y, 0, 0, false, false, false);
+            }
+            if (elapsedSec < 0)
+            {
+                elapsedSec = -100;
             }
             base.Update(gameTime);
         }
